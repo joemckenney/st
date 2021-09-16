fea240b9ec3878b401dd8f997fc012f12a303eac
Joe McKenney <joe@switchboard.cc>
2021-09-16 10:35:48 -0700

    Update font

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=20:antialias=true:autohint=true";
+//static char *font = "Liberation Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Fira Code,Fira Code SemiBold:size=10:style=SemiBold,Regular:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
